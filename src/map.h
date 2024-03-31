edb7409e7f12bfa3d749f31b3706deade63b5ddc
Ole Morud <ole.morud@gmail.com>
2024-04-01 01:33:47 +0200

    asd

--- src/map.h
+++ src/map.h
@@ -1,5 +1,15 @@
 
 
+enum chess_piece {
+    EMPTY  = 0,
+    KING   = 1,
+    QUEEN  = 2,
+    ROOK   = 3,
+    BISHOP = 4,
+    KNIGHT = 5,
+    PAWN   = 6,
+    PIECE_COUNT,
+};
 
 static bool board_equals(
 
